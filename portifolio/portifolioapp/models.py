eb2f0ae3128aa64f03854ca89b61a4937bff4ea0
churysungita <azizisungita5@gmail.com>
2022-04-04 14:52:48 +0300

    djangob commit

--- portifolio/portifolioapp/models.py
+++ portifolio/portifolioapp/models.py
@@ -1,3 +1,12 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+class project (models.Model):
+    title=models.CharField(max_length=50)
+    text=models.TextField(max_length=100)
+    image=models.ImageField(upload_to='media/')
+    link=models.URLField(max_length=50)
+    def _str_(self):
+        return self.title
--- portifolio/portifolioapp/views.py
+++ portifolio/portifolioapp/views.py
@@ -1,9 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 from django.conf import settings
-
+from .models import project
 from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    project=project.objects.all()
+    context={
+        "myproject":project
+    }
+    return render(request, 'index.html',context)
+  
