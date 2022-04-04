357f11de1419159490ae5055d72e9c547687f035
churysungita <azizisungita5@gmail.com>
2022-04-04 11:50:18 +0300

    django2 commit

--- portifolio/portifolio/settings.py
+++ portifolio/portifolio/settings.py
@@ -38,10 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portifolioapp',
-    'crispy_forms',
+    
 ]
 
-CRISPY_TEMPLATE_PACK ='bootstrap3'  
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 MEDIA_URL='/media/'
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
